822347b13079c92773d91750f1144675e6cb14bd
Mutava-Ndinda <ndindafmutava@gmail.com>
2024-07-01 21:48:43 +0300

    made changes

--- app/src/main/java/com/akirachix/myarticles/MainActivity.kt
+++ app/src/main/java/com/akirachix/myarticles/MainActivity.kt
@@ -18,13 +18,13 @@ class MainActivity : AppCompatActivity() {
         displayArticles()
     }
     fun displayArticles(){
-        val article1 = Article("Jordan B Peterson","12 Rules Of Life","16th","one","http")
-        val article2 = Article("Jordan B Peterson","12 Rules Of Life","16th","one","http")
-        val article3 = Article("Jordan B Peterson","12 Rules Of Life","16th","one","http")
-        val article4 = Article("Jordan B Peterson","12 Rules Of Life","16th","one","http")
-        val article5 = Article("Jordan B Peterson","12 Rules Of Life","16th","one","http")
-        val article6 = Article("Jordan B Peterson","12 Rules Of Life","16th","one","http")
-        val article7 = Article("Jordan B Peterson","12 Rules Of Life","16th","one","http")
+        val article1 = Article("Jordan B Peterson","12 Rules Of Life","Published:16th","one.....","View more")
+        val article2 = Article("Jordan B Peterson","12 Rules Of Life","Published:16th","two...","View more")
+        val article3 = Article("Jordan B Peterson","12 Rules Of Life","Published:16th","three...","View more")
+        val article4 = Article("Jordan B Peterson","12 Rules Of Life","Published:16th","four...","View more")
+        val article5 = Article("Jordan B Peterson","12 Rules Of Life","Published:16th","five...","View more")
+        val article6 = Article("Jordan B Peterson","12 Rules Of Life","Published:16th","six...","View more")
+        val article7 = Article("Jordan B Peterson","12 Rules Of Life","Published:16th","seven...","View more")
 
         val articlesList= listOf(article1,article2,article3,article4,article5,article6,article7)
         val articleAdapter = ArticleAdapter(articlesList)
